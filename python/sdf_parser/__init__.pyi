3c4cb90dd42ad7a361436a06f3f00351c8717618
boykopovar <boykopovar@gmail.com>
2026-07-07 12:45:01 +0300

    refactor(lib): update python lib name to sqlce

--- python/sdf_parser/__init__.pyi
+++
@@ -1,5 +0,0 @@
-from sdf_parser._sdf_native import ColumnSchema
-from sdf_parser._sdf_native import Row
-from sdf_parser._sdf_native import SdfDatabase
-
-__all__ = ["ColumnSchema", "Row", "SdfDatabase"]
