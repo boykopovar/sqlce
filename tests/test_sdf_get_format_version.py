7509996f139306d112a45cb87da80de1c9c2d278
boykopovar <boykopovar@gmail.com>
2026-07-21 16:12:10 +0300

    fix(tests): fix get format testing by grouping 35 or 40

--- tests/test_sdf_get_format_version.py
+++ tests/test_sdf_get_format_version.py
@@ -9,27 +9,28 @@ from tests.utils.sdf_factory import SDF_VERSION_40
 from tests.utils.scenarios import ALL_SCENARIO_NAMES
 from tests.utils.scenarios import build_scenario
 
-EXPECTED_FORMAT_VERSION_BY_SDF_VERSION = {
-    SDF_VERSION_35: FormatVersion.SQLCE_35_SP2,
-    SDF_VERSION_40: FormatVersion.SQLCE_40,
+SDF_VERSION_BY_FORMAT_VERSION = {
+    FormatVersion.SQLCE_35: SDF_VERSION_35,
+    FormatVersion.SQLCE_35_SP2: SDF_VERSION_35,
+    FormatVersion.SQLCE_40: SDF_VERSION_40,
 }
 
 
 @pytest.mark.parametrize("scenario_name", ALL_SCENARIO_NAMES)
 def test_sdf_static_format_version_matches_expected(sdf_dir: Path, scenario_name: str) -> None:
     scenario = build_scenario(scenario_name, sdf_dir)
-    expected_version = EXPECTED_FORMAT_VERSION_BY_SDF_VERSION[scenario.version]
+    actual_version = SqlceDatabase.get_format_version_from_file(str(scenario.path))
 
-    assert SqlceDatabase.get_format_version_from_file(str(scenario.path)) == expected_version
+    assert SDF_VERSION_BY_FORMAT_VERSION[actual_version] == scenario.version
 
 
 @pytest.mark.parametrize("scenario_name", ALL_SCENARIO_NAMES)
 def test_sdf_instance_format_version_matches_expected(sdf_dir: Path, scenario_name: str) -> None:
     scenario = build_scenario(scenario_name, sdf_dir)
-    expected_version = EXPECTED_FORMAT_VERSION_BY_SDF_VERSION[scenario.version]
     db = scenario.open_database()
+    actual_version = db.get_format_version()
 
-    assert db.get_format_version() == expected_version
+    assert SDF_VERSION_BY_FORMAT_VERSION[actual_version] == scenario.version
 
 
 @pytest.mark.parametrize("scenario_name", ALL_SCENARIO_NAMES)
